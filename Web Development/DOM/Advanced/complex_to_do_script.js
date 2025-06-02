7299c74f1a927abd161fa9a7b54377707348575e
Noor <noorgirdhar705@gmail.com>
2025-06-02 23:47:01 +0530

    Adding more codes

---
+++ Web Development/DOM/Advanced/complex_to_do_script.js
@@ -0,0 +1,7 @@
+function createComplexDomElement() {
+		const div = document.createElement("div");
+		const h1 = document.createElement("h1");
+		h1.innerHTML = "random text";
+		div.appendChild(h1);
+		document.querySelector("body").appendChild(div);
+	}
