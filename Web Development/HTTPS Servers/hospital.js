e40f5dec9d493dfb7a92022fe6416297a360b024
Noor <noorgirdhar705@gmail.com>
2025-06-05 22:56:03 +0530

    Hospital Backend Assignment

--- Web Development/HTTPS Servers/hospital.js
+++ Web Development/HTTPS Servers/hospital.js
@@ -5,6 +5,8 @@ var users = [{name: "alpha",
     kidneys: [{healthy : false}]
 }]
 
+clinic.use(exp.json());
+
 clinic.get("/", function(req, res){
     const alphaKidney = users[0].kidneys;
     const noOfKidney = alphaKidney.length;
@@ -23,6 +25,75 @@ clinic.get("/", function(req, res){
         noOfKidney, healthyKidneys, unhealthyKidneys 
     });
 
+})
+
+
+clinic.post("/", function(req, res)
+{
+    const isHealthy = req.body.isHealthy;
+    users[0].kidneys.push({
+        healthy: isHealthy
+    })
+    res.json({
+        msg: "Done!"
+    })
+})
+
+// As you send to update kidney(POST), you will see changes in the get request when you try to get those details !!
+
+clinic.put("/", function(req, res)
+{
+    for(let i = 0; i<users[0].kidneys.length; i++)
+    {
+        users[0].kidneys[i].healthy = true;
+    }
+    res.json({});
+})
+
+function isThereAtLeastOnePoorKidney ()
+{
+    let atLeastOneBad = false;
+    for(let i = 0; i<users[0].kidneys.length; i++)
+    {
+        if(!users[0].kidneys[i].healthy)
+        {
+            atLeastOneBad = true;
+        }
+    }
+
+    return atLeastOneBad;
+}
+
+// Delete the unhealthy kidney !!
+clinic.delete("/", function(req,res)
+{
+    // If there are no unhealthy kidney what's the point of doing the surgery !!....so condition add krte hain !!
+    
+    if(isThereAtLeastOnePoorKidney())
+    {
+        const newKidneys = [];
+
+        for(let i = 0; i<users[0].kidneys.length; i++)
+        {
+            if (users[0].kidneys[i].healthy)
+            {
+                newKidneys.push({
+                    healthy: true
+                })
+            }
+        }
+        
+        users[0].kidneys = newKidneys;
+        res.json({msg: "Done !!"})
+    }
+
+    else
+    {
+        res.status(411).json({
+            msg: "You have no bad Kidneys !!"
+        });
+    }
+    
 })
 
 clinic.listen(3002);
